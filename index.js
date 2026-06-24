40fdbde754f65e2325b10faa793dae4363b5a2f7
kin-jayro <2320509@ub.edu.ph>
2026-06-24 22:51:47 +0800

    STARTING

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const closeSideBa= document.getElementById('hide-sidebar')
+
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+
